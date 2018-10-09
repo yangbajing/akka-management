6ed1b7450909b47748d35720289c460e42e1a250
Arnout Engelen <arnouten@bzzt.net>
2018-10-09 18:43:15 +0200

    Allow connecting to the cluster bootstrap probe endpoint by IP

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -149,8 +149,8 @@ object Dependencies {
       DependencyGroups.AkkaCluster ++
       DependencyGroups.AkkaDiscovery ++
       DependencyGroups.AkkaHttpCore ++
-      DependencyGroups.AkkaTesting ++
-      DependencyGroups.AkkaHttpTesting ++ Seq(
+      DependencyGroups.AkkaHttpTesting ++
+      DependencyGroups.AkkaTesting ++ Seq(
         "com.typesafe.akka" %% "akka-distributed-data" % AkkaVersion % "test"
       )
   )
